0a556509acedc815af9e1be1aa35a414f6a7358a
Caryl Wyatt <caryl.e.wyatt@gmail.com>
2019-03-21 16:15:21 -0500

    first test passing... but maybe not great

--- saForm/test.js
+++ saForm/test.js
@@ -1,13 +1,15 @@
 var expect = require('chai').expect,
   { JSDOM } = require('jsdom');
 
-const jsdom = new JSDOM('<!doctype html><html><body><div>1</div></body></html>');
+const jsdom = new JSDOM('<!doctype html><body><div>1</div></body></html>');
 const {window} = jsdom;
 const {document} = window;
 global.window = window;
 global.document = document;
 
 const $ = global.jQuery = require('jquery');
+
+//console.log(document.querySelector('div').textContent); //1
 
 describe('helloWorld', function () {
   var helloWorld = require('./index.js').helloWorld;
@@ -16,3 +18,6 @@ describe('helloWorld', function () {
     expect(helloWorld()).to.have.string('hello world');
   });
 });
+
+// how to test jquery when it isn't really functional programming? 
+// there are *functions* buuuut it's just dom manipulation?
